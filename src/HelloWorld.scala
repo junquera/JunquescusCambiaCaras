52a9548816fdea49fbd80de353ae0dc76926fe54
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2015-03-27 12:28:30 +0100

    Tablero bien hecho

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -6,13 +6,13 @@ object HelloWorld {
 
     println("Hello, world!" + x)
 
-    val punto = new Point(1, 2)
-    println(punto.toString())
-    // http://docs.scala-lang.org/es/tutorials/tour/classes.html
     val mainList = List(List(1,2,3), 2, 1)
     val with4 = 4 :: mainList // re-uses mainList, costs one :: instance
     val with42 = 42 :: mainList // also re-uses mainList, cost one :: instance
     val shorter = mainList(0).asInstanceOf[List[Int]](0)
     println(shorter)
+    
+    val tablero = new Tablero()
+    
   }
 }
--- src/Tablero.scala
+++ src/Tablero.scala
@@ -1,12 +1,28 @@
 import Array._
+import scala.util.Random
 
-class Tablero {
-  val colores = List('R')
+class Tablero (dificultadN: Int) {
+  val colores = 'A' :: 'N' :: 'R' :: 'V' :: 'M' :: 'G' :: 'B' :: List()
+  val dificultad = 3 :: 5 :: 7 :: List()
+  val rnd = new Random()
   
-  var T = ofDim[Char](8, 8)
-  for (i <- 0 to 2) {
-    for (j <- 0 to 2) {
-      T(i)(j) = colores(0);
+  val sizeX = 8
+  val sizeY = 8
+  
+  var T = ofDim[Char](sizeX, sizeY)
+  
+  
+  for (i <- 0 to sizeX) {
+    for (j <- 0 to sizeY) {
+      T(i)(j) = colores(rnd.nextInt(dificultad(dificultadN)));
+    }
+  }
+  
+  def print(){
+    for(i <- 0 to sizeX){
+      for(j <- 0 to sizeY){
+        printf(T(i)(j).toString())
+      }
     }
   }
 }
