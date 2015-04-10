448d1b9800d206bd51b1fad6ed6c479e8dddde5b
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2015-04-10 12:51:10 +0200

    Versión viernes 10-IV-2015

--- src/Tablero.scala
+++ src/Tablero.scala
@@ -1,30 +1,3 @@
-import Array._
-import scala.util.Random
+class Tablero(tabla: List[List[Char]]) {
 
-class Tablero (dificultadN: Int) {
-  val colores = 'A' :: 'N' :: 'R' :: 'V' :: 'M' :: 'G' :: 'B' :: List()
-  val dificultad = 3 :: 5 :: 7 :: List()
-  val rnd = new Random()
-  
-  val sizeX = 8
-  val sizeY = 8
-  
-  var T = ofDim[Char](sizeX, sizeY)
-  
-  
-  for (i <- 0 to sizeX - 1) {
-    for (j <- 0 to sizeY - 1) {
-      T(i)(j) = colores(rnd.nextInt(dificultad(dificultadN)));
-    }
-  }
-  
-  def print(){
-    for(i <- 0 to sizeX - 1){
-      printf("[")
-      for(j <- 0 to sizeY - 1){
-        printf(T(i)(j).toString())
-      }
-      println("]")
-    }
-  }
 }
