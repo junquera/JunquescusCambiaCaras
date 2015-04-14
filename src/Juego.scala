a2e93ce0efa6ffc1f66ad1b118d68b5246e0b812
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2015-04-14 14:46:05 +0200

    Punto y métodos de borrado en tablero

--- src/Juego.scala
+++ src/Juego.scala
@@ -2,20 +2,21 @@ import scala.util.Random
 
 class Juego(dificultadN: Int) {
 
-  val colores = 'A' :: 'N' :: 'R' :: 'V' :: 'M' :: 'G' :: 'B' :: List()
+  val colores = 'A' :: 'N' :: 'R' :: 'V' :: 'M' :: 'G' :: 'B' :: Nil
   val dificultad = 3 :: 5 :: 7 :: Nil
   val rnd = new Random()
-
   val sizeX = 8
   val sizeY = 8
 
-  def creaTableroAleatorio() : Tablero = {
-    return new Tablero(creaTabla(sizeX, sizeY));  
+  // Crear tablero aleatorio
+
+  def creaTableroAleatorio(): Tablero = {
+    return new Tablero(creaTabla(sizeX, sizeY));
   }
-  
+
   // Crear tabla aleatorio con n listas
 
-  def creaTabla(n:Integer, y: Integer): List[List[Char]] = {
+  def creaTabla(n: Integer, y: Integer): List[List[Char]] = {
     if (n == 0)
       return Nil;
     else
@@ -30,14 +31,41 @@ class Juego(dificultadN: Int) {
     else
       return colores(rnd.nextInt(dificultad(dificultadN))) :: creaLista(n.-(1));
   }
-  
 
-  // Imprimir tablero
+  def shorter(l1: List[Char], l2: List[Char]): List[Char] = {
+    if (l1.length < l2.length)
+      return l1
+    else
+      return l2
+  }
 
-  //  Estrategia ganadora
+  def todosIguales(lista: List[Char]): Boolean = {
+    if (lista.isEmpty || lista.tail.isEmpty)
+      return true
+    else
+      return lista.head.equals(lista.tail.head) && todosIguales(lista.tail)
+  }
 
-  //  Recorre horizontal
+  def borraYCrea(lista: List[Char]): ResultadoYPuntos = {
+    val aux = borraCoincidencias(lista)
+    return new ResultadoYPuntos(creaLista(lista.length - aux.length) ::: aux, aux.length)
+  }
 
-  //  Recorre vertical
+  def borraCoincidencias(lista: List[Char]): List[Char] = {
+    return shorter(borraCoincidenciasAux(lista), borraCoincidenciasAux(lista.reverse).reverse)
+  }
 
+  def borraCoincidenciasAux(lista: List[Char]): List[Char] = {
+    if (lista.length < 3)
+      return lista
+    else if (todosIguales(lista))
+      return Nil
+    else
+      return lista.head :: borraCoincidencias(lista.tail)
+  }
+}
+
+class ResultadoYPuntos(resultado: List[Char], puntos: Integer) {
+  val getResultado = resultado
+  val getPuntos = puntos
 }
--- src/Main.scala
+++ src/Main.scala
@@ -1,10 +1,15 @@
 // Empezar con http://docs.scala-lang.org/overviews/
 object HelloWorld {
-  
+
   def main(args: Array[String]) {
 
     val juego = new Juego(2)
+    val t = juego.creaTableroAleatorio()
+    t.print()
 
+    for (x <- juego.borraYCrea(List('X', 'A', 'A', 'A', 'B')).getResultado) {
+      printf(x.toString())
+    }
 
   }
 
