ebcbcf4fc2eeba9ea089b70aa778b1c9def0ec79
Javier Junquera Sánchez <javier.junquera.sanchez@gmail.com>
2015-04-20 16:32:31 +0200

    Juego y principio de memoria

--- src/Juego.scala
+++ src/Juego.scala
@@ -8,6 +8,11 @@ class Juego(dificultadN: Int) {
   val sizeX = 8
   val sizeY = 8
 
+  var puntos = 0
+
+  /**
+   * Imprime el contenido de una tabla de juego
+   */
   def print(tabla: List[List[Char]]) = {
     for (x <- 0 to tabla.size - 1) {
       printf("[ ")
@@ -18,20 +23,41 @@ class Juego(dificultadN: Int) {
     }
   }
 
+  /**
+   * Método que se ejecutará cada turno y llamará a los métodos de juego. Se llama a
+   * sí mismo de forma recursiva hasta que, tras la jugada, no hay fichas que eliminar
+   */
   def turno(tabla: List[List[Char]]): List[List[Char]] = {
     val aux = juegaTurno(tabla)
-
-    if (aux.equals(tabla)) {
+    if (aux.equals(tabla))
       return tabla
-    } else {
+    else
       return turno(aux)
-    }
   }
 
+  /**
+   *  Método de juego. Analiza la tabla, marca con 0 las fichas que hay que eliminar, añade puntos
+   *  al juego y elimina los elementos, rellenando los huecos que queden con colores aleatorios
+   */
   def juegaTurno(tabla: List[List[Char]]): List[List[Char]] = {
-    return compactaYRellena(transpose(borraCoincidencias(transpose(borraCoincidencias(tabla)))))
+    val tablaAnalizada = traspose(borraCoincidencias(traspose(borraCoincidencias(tabla))))
+    puntos.+(cuentaElementos('0', tablaAnalizada))
+    return compactaYRellena(traspose(borraCoincidencias(traspose(borraCoincidencias(tabla)))))
   }
 
+  /**
+   * Cuenta el número de ocurrencias de c en el tablero t
+   */
+  def cuentaElementos(c: Char, tabla: List[List[Char]]): Integer = {
+    if (tabla.isEmpty)
+      return 0
+    else
+      return cuentaElementosLista(c, tabla.head).+(cuentaElementos(c, tabla.tail))
+  }
+
+  /**
+   * Marca como 0 los elementos que se repiten contiguamente 3 o más veces en una tabla.
+   */
   def borraCoincidencias(tabla: List[List[Char]]): List[List[Char]] = {
     if (tabla.isEmpty)
       return Nil
@@ -39,6 +65,9 @@ class Juego(dificultadN: Int) {
       return borraCoincidenciasLista(tabla.head) :: borraCoincidencias(tabla.tail)
   }
 
+  /**
+   * Elimina los ceros y rellena los huecos que quedan tras compactar con colores aleatorios
+   */
   def compactaYRellena(tabla: List[List[Char]]): List[List[Char]] = {
     if (tabla.isEmpty)
       return Nil
@@ -46,30 +75,12 @@ class Juego(dificultadN: Int) {
       return compactaYRellenaLista(tabla.head) :: compactaYRellena(tabla.tail)
   }
 
-  def compactaYRellenaLista(l: List[Char]): List[Char] = {
-    if (l.isEmpty)
-      return Nil
-    else {
-      val aux = compacta(l)
-      return creaLista(l.size - aux.size) ::: aux
-    }
-  }
+  /********************* <TRANSPUESTA> **************/
+  /**
+   * Métodos para hacer la transpuesta de una tabla
+   */
 
-  // Crear tablero aleatorio
-  def creaTableroAleatorio(): Tablero = {
-    return new Tablero(creaTabla(sizeX, sizeY));
-  }
-
-  def getMod(lista: List[Char], i: Integer, mod: Integer): List[Char] = {
-    if (lista.isEmpty)
-      return Nil
-    else if (i.%(8) == mod)
-      return lista.head :: getMod(lista.tail, i.+(1), mod)
-    else
-      return getMod(lista.tail, i.+(1), mod)
-  }
-
-  def transpose(tabla: List[List[Char]]): List[List[Char]] = {
+  def traspose(tabla: List[List[Char]]): List[List[Char]] = {
     val list = tabla.flatten
     return List(getMod(list, 0, 0),
       getMod(list, 0, 1),
@@ -81,7 +92,20 @@ class Juego(dificultadN: Int) {
       getMod(list, 0, 7))
   }
 
-  // Crear tabla aleatorio con n listas
+  def getMod(lista: List[Char], i: Integer, mod: Integer): List[Char] = {
+    if (lista.isEmpty)
+      return Nil
+    else if (i.%(8) == mod)
+      return lista.head :: getMod(lista.tail, i.+(1), mod)
+    else
+      return getMod(lista.tail, i.+(1), mod)
+  }
+
+  /**************** </TRANSPUESTA> *****************/
+
+  /**
+   *  Crea una tabla aleatoria con n columnas e y filas
+   */
   def creaTabla(n: Integer, y: Integer): List[List[Char]] = {
     if (n == 0)
       return Nil;
@@ -89,8 +113,9 @@ class Juego(dificultadN: Int) {
       return creaLista(y) :: creaTabla(n.-(1), y);
   }
 
-  // Crear lista aleatoria n elementos
-
+  /**
+   * Crea una lista aleatoria con n elementos
+   */
   def creaLista(n: Integer): List[Char] = {
     if (n == 0)
       return Nil;
@@ -98,6 +123,19 @@ class Juego(dificultadN: Int) {
       return colores(rnd.nextInt(dificultad(dificultadN))) :: creaLista(n.-(1));
   }
 
+  /**
+   * Crea una lista de n elementos y la rellena con caracteres c
+   */
+  def nuevaLista(c: Char, n: Integer): List[Char] = {
+    if (n == 0)
+      return Nil
+    else
+      return c :: nuevaLista(c, n.-(1))
+  }
+
+  /**
+   * Devuelve la lista más corta
+   */
   def shorter(l1: List[Char], l2: List[Char]): List[Char] = {
     if (l1.length < l2.length)
       return l1
@@ -105,6 +143,11 @@ class Juego(dificultadN: Int) {
       return l2
   }
 
+  /**
+   * Comprueba si todos los elementos de una lista son iguales
+   * Ponemos como marca que no compruebe si el elemento es 0 porque
+   * es el caracter con el que marcaremos los elementos repetidos
+   */
   def todosIguales(lista: List[Char]): Boolean = {
     if (lista.isEmpty || lista.tail.isEmpty)
       return true
@@ -114,31 +157,23 @@ class Juego(dificultadN: Int) {
       return lista.head.equals(lista.tail.head) && todosIguales(lista.tail)
   }
 
-  def borraYCrea(lista: List[Char]): ResultadoYPuntos = {
-    val auxList = borraCoincidenciasLista(lista)
-    val borrados = lista.length - auxList.length
-    return new ResultadoYPuntos(creaLista(borrados) ::: auxList, borrados)
-  }
-
+  /**
+   * Marca con 0 los elementos que hay que eliminar de la lista
+   * (con 3 o más coincidencias contiguas)
+   */
   def borraCoincidenciasLista(lista: List[Char]): List[Char] = {
     val l1 = borraCoincidenciasAux(lista)
     val l2 = borraCoincidenciasAux(lista.reverse).reverse
-    if (cuentaElementos('0', l1) > cuentaElementos('0', l2)) {
+    if (cuentaElementosLista('0', l1) > cuentaElementosLista('0', l2)) {
       return l1
     } else {
       return l2
     }
   }
 
-  def cuentaElementos(c: Char, l: List[Char]): Integer = {
-    if (l == Nil)
-      return 0
-    else if (l.head == c)
-      return 1.+(cuentaElementos(c, l.tail))
-    else
-      return cuentaElementos(c, l.tail)
-  }
-
+  /**
+   * Método auxiliar para ayudar en la recursividad del método borraCoincidenciasLista
+   */
   def borraCoincidenciasAux(lista: List[Char]): List[Char] = {
     if (lista.length < 3)
       return lista
@@ -148,6 +183,21 @@ class Juego(dificultadN: Int) {
       return lista.head :: borraCoincidenciasLista(lista.tail)
   }
 
+  /**
+   * Cuenta el número de repeticiones de c en la lista l
+   */
+  def cuentaElementosLista(c: Char, lista: List[Char]): Integer = {
+    if (lista.isEmpty)
+      return 0
+    else if (lista.head == c)
+      return 1.+(cuentaElementosLista(c, lista.tail))
+    else
+      return cuentaElementosLista(c, lista.tail)
+  }
+
+  /**
+   *  Borra los elementos marcados como 0 en una lista
+   */
   def compacta(lista: List[Char]): List[Char] = {
     if (lista == Nil)
       return Nil
@@ -157,15 +207,80 @@ class Juego(dificultadN: Int) {
       return lista.head :: compacta(lista.tail)
   }
 
-  def nuevaLista(c: Char, n: Integer): List[Char] = {
-    if (n == 0)
+  /**
+   *  Llama al método compacta y rellena la cabeza de la lista con colores aleatorios
+   */
+  def compactaYRellenaLista(lista: List[Char]): List[Char] = {
+    if (lista.isEmpty)
       return Nil
-    else
-      return c :: nuevaLista(c, n.-(1))
+    else {
+      val aux = compacta(lista)
+      return creaLista(lista.size - aux.size) ::: aux
+    }
   }
-}
 
-class ResultadoYPuntos(resultado: List[Char], puntos: Integer) {
-  val getResultado = resultado
-  val getPuntos = puntos
+  /**************** <EXCHANGE> *********************/
+
+  def exchange(tabla: List[List[Char]], x1: Integer, y1: Integer, x2: Integer, y2: Integer): List[List[Char]] = {
+    if (x1.equals(x2)) return exchange_vertical(tabla, x1, y1, x2, y2, 0, 0)
+    else if (y1.equals(y2)) return traspose(exchange_horizontal(traspose(tabla), y1, x1, y2, x2, 0, 0))
+    else return Nil
+  }
+
+  def exchange_vertical(tabla: List[List[Char]], x1: Integer, y1: Integer, x2: Integer, y2: Integer, xAct: Integer, yAct: Integer): List[List[Char]] = {
+    if (xAct < x1) return tabla.head :: exchange_vertical(tabla.tail, x1, y1, x2, y2, xAct.+(1), yAct)
+    else return exchange_row(tabla.head, y1, y2, 0) :: tabla.tail
+  }
+
+  def exchange_horizontal(tabla: List[List[Char]], x1: Integer, y1: Integer, x2: Integer, y2: Integer, xAct: Integer, yAct: Integer): List[List[Char]] = {
+    if (xAct < x1) return tabla.head :: exchange_horizontal(tabla.tail, x1, y1, x2, y2, xAct.+(1), yAct)
+    else return exchange_row(tabla.head, y1, y2, 0) :: tabla.tail
+  }
+
+  def exchange_row(row: List[Char], p1: Integer, p2: Integer, current: Integer): List[Char] = {
+    if (current < p1) return row.head :: exchange_row(row.tail, p1, p2, current.+(1))
+    else return row.tail.head :: row.head :: (row drop 2)
+  }
+
+  /**************** </EXCHANGE> *********************/
+
+  /**************** <CHECK> ***********************/
+
+  def check_horizontal(primera: List[Char], resto: List[List[Char]], x: Integer, y: Integer): List[Integer] = {
+    if ((!resto.isEmpty) && (!primera.isEmpty))
+      if (primera.length <= 2)
+        return check_horizontal(resto.head, resto.tail, x + 1, 0)
+      else if ((primera.tail takeWhile (_ == primera.head)).length >= 2)
+        return List(x, y, (primera.tail takeWhile (_ == primera.head)).length.+(1))
+      else {
+        val ocurrences = (primera.tail takeWhile (_ == primera.head)).length
+        return check_horizontal(primera drop (1 + ocurrences), resto, x, y + 1 + ocurrences)
+      }
+    else
+      return Nil
+  }
+
+  def check_vertical(primera: List[Char], resto: List[List[Char]], x: Integer, y: Integer): List[Integer] = {
+    if ((!resto.isEmpty) && (!primera.isEmpty))
+      if (primera.length <= 2)
+        return check_vertical(resto.head, resto.tail, x + 1, 0)
+      else if ((primera.tail takeWhile (_ == primera.head)).length >= 2)
+        return List(y, x, (primera.tail takeWhile (_ == primera.head)).length.+(1))
+      else {
+        val ocurrences = (primera.tail takeWhile (_ == primera.head)).length
+        return check_vertical(primera drop (1 + ocurrences), resto, x, y + 1 + ocurrences)
+      }
+    else
+      return Nil
+  }
+
+  def check(table: List[List[Char]], tipo: Char): List[Integer] = {
+    if (tipo.equals('H')) return check_horizontal(table.head, table.tail, 0, 0)
+    else if (tipo.equals('V')) {
+      val trasposed = traspose(table)
+      return check_vertical(trasposed.head, trasposed.tail, 0, 0)
+    } else return Nil
+  }
+  
+  /********************* </CHECK> ****************************************/
 }
--- src/Main.scala
+++ src/Main.scala
@@ -3,11 +3,11 @@ object HelloWorld {
 
   def main(args: Array[String]) {
 
-    val juego = new Juego(0)
-    val t = juego.creaTableroAleatorio()
+    val juego = new Juego(2)
+    val t = juego.creaTabla(juego.sizeX.asInstanceOf[Integer], juego.sizeY.asInstanceOf[Integer])
+
     
-    juego.turno(t.getTabla)
-    
+    val x = List('A', 'A','A','B','A','A','A')
   }
 
 }
