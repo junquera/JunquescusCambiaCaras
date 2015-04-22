3883742ab1367ade4e255b6e2f6b665da8ceffba
J.Junquera <javier.junquera.sanchez@gmail.com>
2015-04-22 19:16:38 +0200

    Update MainUI.scala

--- src/com/CambiaCarasAPA/MainUI.scala
+++ src/com/CambiaCarasAPA/MainUI.scala
@@ -61,7 +61,7 @@ class UI(val juego: Juego) extends MainFrame {
   }
 
   def mueve(x1: Int, y1: Int, x2: Int, y2: Int) = {
-    tablaPrincipal = juego.turno(juego.exchange(tablaPrincipal, x1, y1, x2, y2), false)
+    tablaPrincipal = juego.turno(juego.exchange(tablaPrincipal, x1, y1, x2, y2))
     paint(tablaPrincipal)
   }
 
@@ -111,9 +111,11 @@ class UI(val juego: Juego) extends MainFrame {
     try {
       val dificultad = this.dificultad.text.toInt
       if (dificultad.equals(1) || dificultad.equals(2) || dificultad.equals(3)) {
-        val juego = new Juego(2)
+        val juego = new Juego(dificultad)
         tablaPrincipal = juego.creaTabla(8, 8)
         paint(juego.limpiar(tablaPrincipal))
+      } else {
+        Dialog.showMessage(contents.head, "Dificultad inválida", title = "ERROR")
       }
     } catch {
       case e: Exception => Dialog.showMessage(contents.head, "Dificultad inválida", title = "ERROR")
@@ -122,12 +124,12 @@ class UI(val juego: Juego) extends MainFrame {
 
   def turno() {
     val movimiento = juego.find_move(tablaPrincipal)
-    tablaPrincipal = juego.turno(juego.exchange(tablaPrincipal, movimiento(0), movimiento(1), movimiento(2), movimiento(3)), true)
+    tablaPrincipal = juego.turno(juego.exchange(tablaPrincipal, movimiento(0), movimiento(1), movimiento(2), movimiento(3)))
     paint(tablaPrincipal)
   }
 }
 
-object CambiaCaras {
+object CambiaCarasGUI {
   def main(args: Array[String]) {
 
     val juego = new Juego(2)
